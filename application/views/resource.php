8fcd1cef17c6c352e19a85edd3bfefeb6d9f265d
Rizki Ramadhan <66109920+korizki@users.noreply.github.com>
2023-07-16 08:17:59 +0800

    Menambahkan beberapa CDN third party library

---
+++ application/views/resource.php
@@ -0,0 +1,12 @@
+<head>
+   <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="./global.css" rel="stylesheet">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   <link href="https://cdn.datatables.net/v/dt/jq-3.7.0/dt-1.13.5/b-2.4.1/b-html5-2.4.1/datatables.min.css" rel="stylesheet"/>
+   <script src="https://cdn.datatables.net/v/dt/jq-3.7.0/dt-1.13.5/b-2.4.1/b-html5-2.4.1/datatables.min.js"></script>
+
+</head>
+<div>
+   <h1>halo</h1>
+</div>
