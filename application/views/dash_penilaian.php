809e43dbcee98c70d6cb754eee2b4d87831d6036
mooncat26 <krisnas311@gmail.com>
2023-07-18 11:12:03 +0800

    new file:   application/controllers/Penilaian.php 	new file:   application/views/dash_penilaian.php

---
+++ application/views/dash_penilaian.php
@@ -0,0 +1,13 @@
+<?php
+	defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<body>
+	<div id="container">
+		<div>
+			<h1>Dashboard Penilaian</h1>
+		</div>
+	</div>
+
+</body>
+</html>
