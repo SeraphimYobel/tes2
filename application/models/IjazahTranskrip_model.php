d02a053ca0ae5e21dc6824146df1c447c2c834ad
Iwan Aslich <74340158+aslich86@users.noreply.github.com>
2023-07-19 19:13:16 +0700

    Update IjazahTranskrip_model.php
    Menambahkan quey CRUD mulai line 61

--- application/models/IjazahTranskrip_model.php
+++ application/models/IjazahTranskrip_model.php
@@ -57,4 +57,57 @@ class Ijazah_transkrip_model extends CI_Model {
         $this->db->delete('Transkrip_Nilai');
         return $this->db->affected_rows();
     }
+
+	class IjazahTranskrip_model extends CI_Model {
+    public function create_ijazah($data) {
+        $this->db->insert('Ijazah', $data);
+        return $this->db->insert_id();
+    }
+
+    public function read_ijazah($id = null) {
+        if ($id) {
+            return $this->db->get_where('Ijazah', ['ID' => $id])->row_array();
+        } else {
+            return $this->db->get('Ijazah')->result_array();
+        }
+    }
+
+    public function update_ijazah($id, $data) {
+        $this->db->where('ID', $id);
+        $this->db->update('Ijazah', $data);
+        return $this->db->affected_rows();
+    }
+
+    public function delete_ijazah($id) {
+        $this->db->where('ID', $id);
+        $this->db->delete('Ijazah');
+        return $this->db->affected_rows();
+    }
+
+    public function create_transkrip($data) {
+        $this->db->insert('TranskripNilai', $data);
+        return $this->db->insert_id();
+    }
+
+    public function read_transkrip($id = null) {
+        if ($id) {
+            return $this->db->get_where('TranskripNilai', ['ID' => $id])->row_array();
+        } else {
+            return $this->db->get('TranskripNilai')->result_array();
+        }
+    }
+
+    public function update_transkrip($id, $data) {
+        $this->db->where('ID', $id);
+        $this->db->update('TranskripNilai', $data);
+        return $this->db->affected_rows();
+    }
+
+    public function delete_transkrip($id) {
+        $this->db->where('ID', $id);
+        $this->db->delete('TranskripNilai');
+        return $this->db->affected_rows();
+    }
+}
+
 }
