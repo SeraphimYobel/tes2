fbc83f3c6d06f1bd5ce111f18889fcc6b76fff64
Iwan Aslich <74340158+aslich86@users.noreply.github.com>
2023-07-19 19:14:33 +0700

    Update Kota_model.php
    Menambahkan query CRUD mulai line 35

--- application/models/Kota_model.php
+++ application/models/Kota_model.php
@@ -1,3 +1,5 @@
+<?php
+
 // Kota_model.php
 class Kota_model extends CI_Model {
     public function __construct() {
@@ -29,4 +31,32 @@ class Kota_model extends CI_Model {
         $this->db->delete('Kota');
         return $this->db->affected_rows();
     }
+
+	class Kota_model extends CI_Model {
+    public function create_kota($data) {
+        $this->db->insert('Kota', $data);
+        return $this->db->insert_id();
+    }
+
+    public function read_kota($id = null) {
+        if ($id) {
+            return $this->db->get_where('Kota', ['ID' => $id])->row_array();
+        } else {
+            return $this->db->get('Kota')->result_array();
+        }
+    }
+
+    public function update_kota($id, $data) {
+        $this->db->where('ID', $id);
+        $this->db->update('Kota', $data);
+        return $this->db->affected_rows();
+    }
+
+    public function delete_kota($id) {
+        $this->db->where('ID', $id);
+        $this->db->delete('Kota');
+        return $this->db->affected_rows();
+    }
+}
+
 }
