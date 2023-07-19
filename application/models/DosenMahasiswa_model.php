f6289de73b588c9f98c0f1c9e71092c1cfafc3cc
Iwan Aslich <74340158+aslich86@users.noreply.github.com>
2023-07-19 19:11:24 +0700

    Update DosenMahasiswa_model.php
    Menambahkan quey CRUD mulai line 61

--- application/models/DosenMahasiswa_model.php
+++ application/models/DosenMahasiswa_model.php
@@ -57,4 +57,57 @@ class Dosen_mahasiswa_model extends CI_Model {
         $this->db->delete('Mahasiswa');
         return $this->db->affected_rows();
     }
+
+	class DosenMahasiswa_model extends CI_Model {
+    public function create_dosen($data) {
+        $this->db->insert('Dosen', $data);
+        return $this->db->insert_id();
+    }
+
+    public function read_dosen($id = null) {
+        if ($id) {
+            return $this->db->get_where('Dosen', ['ID' => $id])->row_array();
+        } else {
+            return $this->db->get('Dosen')->result_array();
+        }
+    }
+
+    public function update_dosen($id, $data) {
+        $this->db->where('ID', $id);
+        $this->db->update('Dosen', $data);
+        return $this->db->affected_rows();
+    }
+
+    public function delete_dosen($id) {
+        $this->db->where('ID', $id);
+        $this->db->delete('Dosen');
+        return $this->db->affected_rows();
+    }
+
+    public function create_mahasiswa($data) {
+        $this->db->insert('Mahasiswa', $data);
+        return $this->db->insert_id();
+    }
+
+    public function read_mahasiswa($id = null) {
+        if ($id) {
+            return $this->db->get_where('Mahasiswa', ['ID' => $id])->row_array();
+        } else {
+            return $this->db->get('Mahasiswa')->result_array();
+        }
+    }
+
+    public function update_mahasiswa($id, $data) {
+        $this->db->where('ID', $id);
+        $this->db->update('Mahasiswa', $data);
+        return $this->db->affected_rows();
+    }
+
+    public function delete_mahasiswa($id) {
+        $this->db->where('ID', $id);
+        $this->db->delete('Mahasiswa');
+        return $this->db->affected_rows();
+    }
+}
+
 }
