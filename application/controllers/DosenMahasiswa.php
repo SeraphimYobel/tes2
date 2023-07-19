21053e92f2b97b98b233e8ffb84f8f8b756b082a
Iwan Aslich <74340158+aslich86@users.noreply.github.com>
2023-07-19 18:39:30 +0700

    Update DosenMahasiswa.php
    Memasukkan method mulai line 29

--- application/controllers/DosenMahasiswa.php
+++ application/controllers/DosenMahasiswa.php
@@ -25,4 +25,47 @@ class DosenMahasiswa extends CI_Controller {
 		$this->load->view('nav');
 		$this->load->view('dash_dosen_mahasiswa');
 	}
+
+	// Dosen
+public function create_dosen() {
+    // Tampilkan halaman form tambah data dosen
+}
+
+public function store_dosen() {
+    // Proses tambah data dosen ke database
+}
+
+public function edit_dosen($id) {
+    // Tampilkan halaman form edit data dosen berdasarkan ID
+}
+
+public function update_dosen($id) {
+    // Proses update data dosen ke database berdasarkan ID
+}
+
+public function delete_dosen($id) {
+    // Proses hapus data dosen dari database berdasarkan ID
+}
+
+// Mahasiswa
+public function create_mahasiswa() {
+    // Tampilkan halaman form tambah data mahasiswa
+}
+
+public function store_mahasiswa() {
+    // Proses tambah data mahasiswa ke database
+}
+
+public function edit_mahasiswa($id) {
+    // Tampilkan halaman form edit data mahasiswa berdasarkan ID
+}
+
+public function update_mahasiswa($id) {
+    // Proses update data mahasiswa ke database berdasarkan ID
+}
+
+public function delete_mahasiswa($id) {
+    // Proses hapus data mahasiswa dari database berdasarkan ID
+}
+
 }
