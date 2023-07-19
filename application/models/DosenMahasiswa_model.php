4239903caaa42bc381fda4ba22e3c55ad6654848
Joanneeldy <joannelthy@gmail.com>
2023-07-19 14:19:23 +0700

    fixing error pada ci

---
+++ application/models/DosenMahasiswa_model.php
@@ -0,0 +1,60 @@
+<?php
+// DosenMahasiswa_model.php
+class Dosen_mahasiswa_model extends CI_Model {
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    // Operasi pada tabel Dosen
+    public function create_dosen($data) {
+        $this->db->insert('Dosen', $data);
+        return $this->db->insert_id();
+    }
+
+    public function read_dosen($id = null) {
+        if ($id) {
+            return $this->db->get_where('Dosen', ['ID' => $id])->row_array();
+        } else {
+            return $this->db->get('Dosen')->result_array();
+        }
+    }
+
+    public function update_dosen($id, $data) {
+        $this->db->where('ID', $id);
+        $this->db->update('Dosen', $data);
+        return $this->db->affected_rows();
+    }
+
+    public function delete_dosen($id) {
+        $this->db->where('ID', $id);
+        $this->db->delete('Dosen');
+        return $this->db->affected_rows();
+    }
+
+    // Operasi pada tabel Mahasiswa
+    public function create_mahasiswa($data) {
+        $this->db->insert('Mahasiswa', $data);
+        return $this->db->insert_id();
+    }
+
+    public function read_mahasiswa($id = null) {
+        if ($id) {
+            return $this->db->get_where('Mahasiswa', ['ID' => $id])->row_array();
+        } else {
+            return $this->db->get('Mahasiswa')->result_array();
+        }
+    }
+
+    public function update_mahasiswa($id, $data) {
+        $this->db->where('ID', $id);
+        $this->db->update('Mahasiswa', $data);
+        return $this->db->affected_rows();
+    }
+
+    public function delete_mahasiswa($id) {
+        $this->db->where('ID', $id);
+        $this->db->delete('Mahasiswa');
+        return $this->db->affected_rows();
+    }
+}
---
+++ application/views/welcome_message.php
@@ -0,0 +1,34 @@
+<style>
+	.boximage{
+		margin-top: 2em;
+	}
+	.boximage>img{
+		width: 40em;
+		margin-bottom: 2em;
+	}
+	h1{
+		font-size: 2em;
+		margin-top: 0.5em;
+	}
+	h1>strong{
+		color: #1B9C85;
+		font-size: inherit;
+	}
+	#container>div>p{
+		margin-top: 0.5em;
+		opacity: 0.6;
+	}
+	.boximage{
+		display: flex;
+		justify-content: center;
+	}
+</style>
+<div id="container">
+	<div>
+		<h1>Selamat Datang, <strong>Administrator</strong></h1>
+		<p>Silahkan pilih pada <strong>Sidebar</strong> untuk mengakses berbagai menu.</p>
+		<div class="boximage">
+			<img src="../assets/dashboard.png" alt="illustration" />
+		</div>
+	</div>
+</div>
