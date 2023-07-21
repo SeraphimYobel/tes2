569d5045c30fcfb218f265f5455342c8afa99f18
Joanneeldy <joannelthy@gmail.com>
2023-07-21 21:03:42 +0700

    update database

--- siakad.sql
+++ siakad.sql
@@ -3,27 +3,26 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 15, 2023 at 11:52 AM
+-- Generation Time: Jul 21, 2023 at 04:02 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
-;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
-;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
-;
-/*!40101 SET NAMES utf8mb4 */
-;
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `siakad`
-CREATE DATABASE siakad;
-use siakad;
 --
 
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `ijazah`
 --
@@ -42,8 +41,10 @@ CREATE TABLE `ijazah` (
   `nomor_seri` varchar(255) NOT NULL,
   `tanggal_yudisium` date NOT NULL,
   `judul_kkw` varchar(255) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `kota`
 --
@@ -52,8 +53,24 @@ CREATE TABLE `kota` (
   `id` int(11) NOT NULL,
   `kode_kota` varchar(255) NOT NULL,
   `nama` varchar(255) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kota`
+--
+
+INSERT INTO `kota` (`id`, `kode_kota`, `nama`) VALUES
+(1, 'BTM', 'Batam'),
+(2, 'JKT', 'Jakarta'),
+(3, 'BDG', 'Bandung'),
+(4, 'MDN', 'Medan'),
+(5, 'YGY', 'Yogyakarta'),
+(6, 'KDS', 'Kudus'),
+(7, 'PLG', 'Palembang'),
+(8, 'BWI', 'Banyuwangi');
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `matakuliah`
 --
@@ -63,18 +80,18 @@ CREATE TABLE `matakuliah` (
   `kode` varchar(255) NOT NULL,
   `matakuliah` varchar(255) NOT NULL,
   `sks` int(11) NOT NULL,
-  `nilai_angka` int(11) NOT NULL,
-  `nilai_huruf` varchar(255) NOT NULL,
-  `semester` enum(
-    'Semester I',
-    'Semester II',
-    'Semester III',
-    'Semester IV',
-    'Semester V',
-    'Semester VI'
-  ) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `semester` enum('Semester I','Semester II','Semester III','Semester IV','Semester V','Semester VI') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `matakuliah`
+--
+
+INSERT INTO `matakuliah` (`id`, `kode`, `matakuliah`, `sks`, `semester`) VALUES
+(2, '200302215', 'Pemrograman Web II', 3, 'Semester IV');
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `nilai`
 --
@@ -85,8 +102,10 @@ CREATE TABLE `nilai` (
   `nilai_angka` int(11) NOT NULL,
   `nilai_huruf` varchar(255) NOT NULL,
   `matakuliah` int(11) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `pejabat`
 --
@@ -94,11 +113,20 @@ CREATE TABLE `nilai` (
 CREATE TABLE `pejabat` (
   `id` int(11) NOT NULL,
   `nama` varchar(255) NOT NULL,
-  `nip` varchar(255) NOT NULL,
+  `nidn` varchar(255) NOT NULL,
   `golongan` varchar(255) NOT NULL,
   `jabatan` varchar(255) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pejabat`
+--
+
+INSERT INTO `pejabat` (`id`, `nama`, `nidn`, `golongan`, `jabatan`) VALUES
+(1, 'RADEN MUHAMAD FIRZATULLAH', '0306049401', '1A', 'Asisten Ahli');
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `program_studi`
 --
@@ -106,11 +134,20 @@ CREATE TABLE `pejabat` (
 CREATE TABLE `program_studi` (
   `id` int(11) NOT NULL,
   `nama` varchar(255) NOT NULL,
-  `program_pendidikan` enum('Diploma III', 'Diploma IV', 'Sarjana') NOT NULL,
-  `akreditasi` enum('Baik', 'Baik Sekali', 'Unggul') NOT NULL,
+  `program_pendidikan` enum('Diploma III','Diploma IV') NOT NULL,
+  `akreditasi` enum('Baik','Baik Sekali','Unggul') NOT NULL,
   `sk_akreditasi` varchar(255) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `program_studi`
+--
+
+INSERT INTO `program_studi` (`id`, `nama`, `program_pendidikan`, `akreditasi`, `sk_akreditasi`) VALUES
+(1, 'Informatika', 'Diploma III', 'Baik Sekali', '001/AKRED/TI/UNSIA');
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `taruna`
 --
@@ -123,8 +160,20 @@ CREATE TABLE `taruna` (
   `tanggal_lahir` date NOT NULL,
   `program_studi` int(11) NOT NULL,
   `foto` varchar(255) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `taruna`
+--
+
+INSERT INTO `taruna` (`id`, `nama`, `nomor_taruna`, `tempat_lahir`, `tanggal_lahir`, `program_studi`, `foto`) VALUES
+(2, 'Joanne Landy Tantreece', '210401010022', 1, '2003-10-18', 1, 'file'),
+(3, 'Iwan Aslich', '210401010043', 6, '1986-01-23', 1, 'file'),
+(4, 'Rizki Ramadhan', '220401020003', 7, '1993-02-24', 1, 'file'),
+(5, 'Krisna Krisdianto Saputra ', '200401072028', 8, '2002-10-26', 1, 'file');
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `transkip_nilai`
 --
@@ -134,7 +183,8 @@ CREATE TABLE `transkip_nilai` (
   `taruna` int(11) NOT NULL,
   `ijazah` int(11) NOT NULL,
   `program_studi` int(11) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 --
 -- Indexes for dumped tables
 --
@@ -143,53 +193,61 @@ CREATE TABLE `transkip_nilai` (
 -- Indexes for table `ijazah`
 --
 ALTER TABLE `ijazah`
-ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `fk_ijazah_taruna` (`taruna`),
   ADD KEY `fk_ijazah_program_studi` (`program_studi`),
   ADD KEY `fk_ijazah_wakil_direktur` (`wakil_direktur`),
   ADD KEY `fk_ijazah_direktur` (`direktur`);
+
 --
 -- Indexes for table `kota`
 --
 ALTER TABLE `kota`
-ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
 --
 -- Indexes for table `matakuliah`
 --
 ALTER TABLE `matakuliah`
-ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
 --
 -- Indexes for table `nilai`
 --
 ALTER TABLE `nilai`
-ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `fk_nilai_taruna` (`taruna`),
   ADD KEY `fk_nilai_matakuliah` (`matakuliah`);
+
 --
 -- Indexes for table `pejabat`
 --
 ALTER TABLE `pejabat`
-ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
 --
 -- Indexes for table `program_studi`
 --
 ALTER TABLE `program_studi`
-ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
 --
 -- Indexes for table `taruna`
 --
 ALTER TABLE `taruna`
-ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `fk_taruna_kota` (`tempat_lahir`),
   ADD KEY `fk_taruna_program_studi` (`program_studi`);
+
 --
 -- Indexes for table `transkip_nilai`
 --
 ALTER TABLE `transkip_nilai`
-ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `fk_transkip_taruna` (`taruna`),
   ADD KEY `fk_transkip_ijazah` (`ijazah`),
   ADD KEY `fk_transkip_program_studi` (`program_studi`);
+
 --
 -- AUTO_INCREMENT for dumped tables
 --
@@ -198,42 +256,50 @@ ADD PRIMARY KEY (`id`),
 -- AUTO_INCREMENT for table `ijazah`
 --
 ALTER TABLE `ijazah`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `kota`
 --
 ALTER TABLE `kota`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
 --
 -- AUTO_INCREMENT for table `matakuliah`
 --
 ALTER TABLE `matakuliah`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
 -- AUTO_INCREMENT for table `nilai`
 --
 ALTER TABLE `nilai`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `pejabat`
 --
 ALTER TABLE `pejabat`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `program_studi`
 --
 ALTER TABLE `program_studi`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `taruna`
 --
 ALTER TABLE `taruna`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT for table `transkip_nilai`
 --
 ALTER TABLE `transkip_nilai`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- Constraints for dumped tables
 --
@@ -242,33 +308,34 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 -- Constraints for table `ijazah`
 --
 ALTER TABLE `ijazah`
-ADD CONSTRAINT `fk_ijazah_direktur` FOREIGN KEY (`direktur`) REFERENCES `pejabat` (`id`),
+  ADD CONSTRAINT `fk_ijazah_direktur` FOREIGN KEY (`direktur`) REFERENCES `pejabat` (`id`),
   ADD CONSTRAINT `fk_ijazah_program_studi` FOREIGN KEY (`program_studi`) REFERENCES `program_studi` (`id`),
   ADD CONSTRAINT `fk_ijazah_taruna` FOREIGN KEY (`taruna`) REFERENCES `taruna` (`id`),
   ADD CONSTRAINT `fk_ijazah_wakil_direktur` FOREIGN KEY (`wakil_direktur`) REFERENCES `pejabat` (`id`);
+
 --
 -- Constraints for table `nilai`
 --
 ALTER TABLE `nilai`
-ADD CONSTRAINT `fk_nilai_matakuliah` FOREIGN KEY (`matakuliah`) REFERENCES `matakuliah` (`id`),
+  ADD CONSTRAINT `fk_nilai_matakuliah` FOREIGN KEY (`matakuliah`) REFERENCES `matakuliah` (`id`),
   ADD CONSTRAINT `fk_nilai_taruna` FOREIGN KEY (`taruna`) REFERENCES `taruna` (`id`);
+
 --
 -- Constraints for table `taruna`
 --
 ALTER TABLE `taruna`
-ADD CONSTRAINT `fk_taruna_kota` FOREIGN KEY (`tempat_lahir`) REFERENCES `kota` (`id`),
+  ADD CONSTRAINT `fk_taruna_kota` FOREIGN KEY (`tempat_lahir`) REFERENCES `kota` (`id`),
   ADD CONSTRAINT `fk_taruna_program_studi` FOREIGN KEY (`program_studi`) REFERENCES `program_studi` (`id`);
+
 --
 -- Constraints for table `transkip_nilai`
 --
 ALTER TABLE `transkip_nilai`
-ADD CONSTRAINT `fk_transkip_ijazah` FOREIGN KEY (`ijazah`) REFERENCES `ijazah` (`id`),
+  ADD CONSTRAINT `fk_transkip_ijazah` FOREIGN KEY (`ijazah`) REFERENCES `ijazah` (`id`),
   ADD CONSTRAINT `fk_transkip_program_studi` FOREIGN KEY (`program_studi`) REFERENCES `program_studi` (`id`),
   ADD CONSTRAINT `fk_transkip_taruna` FOREIGN KEY (`taruna`) REFERENCES `taruna` (`id`);
 COMMIT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
-;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
-;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
-;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
