c811ee03fd6100c9b8f6d8b165153bd2a84f7820
Joanneeldy <joannelthy@gmail.com>
2023-07-22 10:16:55 +0700

    tampilan transkrip

---
+++ application/views/view_transkrip.php
@@ -0,0 +1,799 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Transkrip Nilai</title>
+  <style>
+    table {
+      width: 100%;
+      border-collapse: collapse;
+    }
+
+    th {
+      border: 1px solid black;
+      padding: 8px;
+      font-weight: bold;
+      border-top: 3px double black;
+    }
+
+    td {
+      
+      padding: 8px;
+    }
+    .table-bor td {
+      border-right: 1px solid black;
+      border-left: 1px solid black;
+    }
+
+    caption {
+      font-size: 1.2em;
+      font-weight: bold;
+      margin-bottom: 10px;
+    }
+
+    .table-container {
+      display: flex;
+      justify-content: space-between;
+      text-align: center;
+    }
+
+    .table-container table:first-child {
+      margin-right: 20px; /* Menambahkan jarak horizontal di antara tabel */
+    }
+
+    .table-name .Matkul {
+      text-align: left;
+
+    }
+    .Matkul{
+      text-align: left;
+    }
+   .tengah{
+      text-align: center; /* Mengatur nilai kolom nomor, usia, dan kota terpusat */
+    }
+
+    h2{
+      text-align: center;
+    }
+
+    
+    .table {
+      display: flex;
+      max-width: 600px;
+    }
+    
+    .cell {
+      flex-grow: 1;
+      border: 1px solid black;
+      padding: 10px;
+    }
+
+    .box {
+            width: 150px;
+            height: 160px;
+            border: 2px solid #000;
+            justify-content: center;
+            align-items: center;
+            font-size: 24px;
+            margin-right: "40px";
+        }
+
+  </style>
+</head>
+<body>
+      <table  style="width:25%; margin-left:40px">
+      <tr>
+        <td>Lampiran Ijazah Nomor</td>
+        <td>: 123</td>
+      </tr>
+      </table>
+      <h2><u>TRANSKRIP NILAI AKADEMIK </u></h2>
+
+  <!-- BIODATA -->
+    <table class="table-name" style="width:60%; margin-left:40px" >
+      <tr>
+        <td>NAMA</td>
+        <td>: Nohara Sinosuke</td>
+      </tr>
+      <tr>
+        <td>NOMOR TARUNA</td>
+        <td>: 123</td>
+      </tr>
+      <tr>
+        <td>TEMPAT TANGGAL/ LAHIR</td>
+        <td>: Bogor, 25 November 1985</td>
+      </tr>
+      <tr>
+        <td>JURUSAN/ PROGRAM STUDY</td>
+        <td>: DIPLOMA III MANAJEMEN TRANSPORTASI PERAIRAN DARATAN</td>
+      </tr>
+      <tr>
+        <td>STATUS</td>
+        <td>: TERAKREDITASI "BAIK SEKALI"</td>
+      </tr>
+      <tr>
+        <td>TANGGAL YUDISIUM</td>
+        <td>: 22 AGUSTUS 2023"</td>
+      </tr>
+  </table>
+  <br/>
+
+
+  <div class="table-container">
+      <!-- TABEL A -->
+    <table class="table-bor">
+      <tr>
+        <th>NO</th>
+        <th>KODE</th>
+        <th class="Matkul">MATA KULIAH</th>
+        <th>SKS</th>
+        <th>NILAI</th>
+      </tr>
+      <tr>
+        <td ></td>
+        <td ></td>
+        <td><b>SEMESTER I (20 SKS)</b></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+
+
+      
+      
+      <tr>
+        <td >1</td>
+        <td >PK103</td>
+        <td class="Matkul">PBB DAN TATA UPACARA</td>
+        <td>2</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td >2</td>
+        <td >KK104</td>
+        <td class="Matkul">BAHASA INGGRIS</td>
+        <td>3</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td >3</td>
+        <td >KK106</td>
+        <td class="Matkul">FISIKA TERAPAN</td>
+        <td>3</td>
+        <td>AB</td>
+      </tr>
+      <tr>
+        <td >4</td>
+        <td >PK102</td>
+        <td class="Matkul">PANCASILA</td>
+        <td>3</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>PK101</td>
+        <td class="Matkul">AGAMA</td>
+        <td>2</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>6</td>
+        <td>KK109</td>
+        <td class="Matkul">MENGGAMBAR TEKNIK</td>
+        <td>2</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>7</td>
+        <td>KK111</td>
+        <td class="Matkul">KOMPUTER</td>
+        <td>2</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>8</td>
+        <td>KK107</td>
+        <td class="Matkul">MATEMATIKA TERAPAN</td>
+        <td>2</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td ></td>
+        <td ></td>
+        <td><b>SEMESTER II (21 SKS)</b></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>9</td>
+        <td>KK220</td>
+        <td class="Matkul">DASAR DASAR TRANSPORTASI</td>
+        <td>2</td>
+        <td>AB</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>10</td>
+        <td>KK218</td>
+        <td class="Matkul">HIDROLOGI</td>
+        <td>2</td>
+        <td>AB</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>11</td>
+        <td>KB225</td>
+        <td class="Matkul">TEKNIK PENGUKURAN DAN PEMETAAN</td>
+        <td>3</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>12</td>
+        <td>KB228</td>
+        <td class="Matkul">ILMU BANGUNAN KAPAL</td>
+        <td>3</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>13</td>
+        <td>PK205</td>
+        <td class="Matkul">BAHASA INDONESIA</td>
+        <td>2</td>
+        <td>AB</td>
+      </tr>
+      <tr>
+        <td>14</td>
+        <td>KK208</td>
+        <td class="Matkul">STATISTIK TERAPAN</td>
+        <td>3</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>15</td>
+        <td>KK213</td>
+        <td class="Matkul">PENGANTAR ILMU HUKUM</td>
+        <td>2</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>16</td>
+        <td>BB245</td>
+        <td class="Matkul">PENGANTAR ILMU HUKUM</td>
+        <td>2</td>
+        <td>A</td>
+      </tr>
+      <tr>
+        <td>17</td>
+        <td>KK219</td>
+        <td class="Matkul">REKAYASA SUNGAI</td>
+        <td>2</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      
+      <tr>
+        <td ></td>
+        <td ></td>
+        <td><b>SEMESTER III (22 SKS)</b></td>
+        <td></td>
+        <td></td>
+      </tr>
+
+      <tr>
+        <td>18</td>
+        <td>KB326</td>
+        <td class="Matkul">TEKNIK SURVEI LLASDP</td>
+        <td>3</td>
+        <td>AB</td>
+      </tr>
+      <tr>
+        <td>19</td>
+        <td>KB324</td>
+        <td class="Matkul">TEKNIK SURVEI HIDROGRAFI</td>
+        <td>3</td>
+        <td>AB</td>
+      </tr>
+      <tr>
+        <td>20</td>
+        <td>KK310</td>
+        <td class="Matkul">DASAR-DASAR EKONOMI</td>
+        <td>2</td>
+        <td>A</td>
+      </tr>     
+      <tr>
+        <td>21</td>
+        <td>KB332</td>
+        <td class="Matkul">TEKNIK ALUR PELAYARAN</td>
+        <td>3</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>22</td>
+        <td>KK327</td>
+        <td class="Matkul">PERMESINAN DAN KELISTRIKAN KAPAL</td>
+        <td>3</td>
+        <td>AB</td>
+      </tr>
+      <tr>
+        <td>23</td>
+        <td>KK131</td>
+        <td class="Matkul">PENGANTAR ILMU MANEJEMEN</td>
+        <td>2</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>24</td>
+        <td>KK316</td>
+        <td class="Matkul">OLAH GERAK KAPAL</td>
+        <td>3</td>
+        <td>AB</td>
+      </tr>
+      <tr>
+        <td>25</td>
+        <td>KB329</td>
+        <td class="Matkul">STABILITAS DAN TEKNIK PEMUATAN</td>
+        <td>3</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr  style="border-bottom: 1px solid black; border-bottom: 3px double #000">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </table>
+
+    <table class="table-bor">
+      <tr>
+        <th>NO</th>
+        <th>KODE</th>
+        <th class="Matkul">MATA KULIAH</th>
+        <th>SKS</th>
+        <th>NILAI</th>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td><b>SEMESTER IV (20 SKS)</b></td>
+        <td></td>
+        <td></td>
+      </tr>
+
+      <tr>
+        <td>26</td>
+        <td>KB431</td>
+        <td class="Matkul">PELAKSANAAN DAN PERANCANGAN PELABUHAN SDP</td>
+        <td>3</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>27</td>
+        <td>KB430</td>
+        <td class="Matkul">TANDA DAN RAMBU PELAYARAN SDP</td>
+        <td>2</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>28</td>
+        <td>KB433</td>
+        <td class="Matkul">MANAJEMEN OPERASI TRANSPOERTASI SDP</td>
+        <td>3</td>
+        <td>AB</td>
+      </tr>
+      <tr>
+        <td>29</td>
+        <td>KB435</td>
+        <td class="Matkul">KESELAMATAN PELAYARAN</td>
+        <td>2</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>30</td>
+        <td>KB434</td>
+        <td class="Matkul">PERENCANAAN TRANSPORT SDP</td>
+        <td>3</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td>31</td>
+        <td>KB436</td>
+        <td class="Matkul">EKONOMI TRANSPORT</td>
+        <td>2</td>
+        <td>A</td>
+      </tr>
+      <tr>
+        <td>32</td>
+        <td>KB439</td>
+        <td class="Matkul">TEKNIK PENGERUKAN </td>
+        <td>3</td>
+        <td>A</td>
+      </tr>
+      <tr>
+        <td>33</td>
+        <td>KK417</td>
+        <td class="Matkul">AMDAL TSDP </td>
+        <td>3</td>
+        <td>A</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td><b>SEMESTER V (19 SKS)</b></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+      <td>34</td>
+        <td>KB537</td>
+        <td class="Matkul">TEKNIK ANALISA EKONOMI DAN FINANSIAL TRANSPORTASI </td>
+        <td>2</td>
+        <td>AB</td>
+      </tr>
+      <td>34</td>
+        <td>KB537</td>
+        <td class="Matkul">TEKNIK ANALISA EKONOMI DAN FINANSIAL TRANSPORTASI </td>
+        <td>2</td>
+        <td>AB</td>
+      </tr>
+      <td>35</td>
+        <td>BB544</td>
+        <td class="Matkul">PERATURAN KESYABANDARAN</td>
+        <td>2</td>
+        <td>AB</td>
+      </tr>
+      <td>36</td>
+        <td>KB537</td>
+        <td class="Matkul">TEKNIK ANALISA EKONOMI DAN FINANSIAL TRANSPORTASI </td>
+        <td>2</td>
+        <td>A</td>
+      </tr>
+      <td>37</td>
+        <td>KB523</td>
+        <td class="Matkul">KAPITA SELECTA</td>
+        <td>2</td>
+        <td>AB</td>
+      </tr>
+      <td>38</td>
+        <td>KB514</td>
+        <td class="Matkul">ILMU ADMINISTRASI</td>
+        <td>2</td>
+        <td>A</td>
+      </tr>
+      <tr>
+        <td>39</td>
+        <td>KK533</td>
+        <td class="Matkul">MANAJEMEN OPERASI PELABUHAN SDP</td>
+        <td>3</td>
+        <td>A</td>
+      </tr>
+      <td>40</td>
+        <td>KK521</td>
+        <td  class="Matkul">TRANSPORTASI MULTIMODA</td>
+        <td>2</td>
+        <td>B</td>
+      </tr>
+      <td>41</td>
+        <td>KB552</td>
+        <td  class="Matkul">METODE PENILITIAN</td>
+        <td>2</td>
+        <td>A</td>
+      </tr>
+      <td>42</td>
+        <td>KB515</td>
+        <td  class="Matkul">KETERAMPILAN DASAR KESELAMATAN</td>
+        <td>2</td>
+        <td>A</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td><b>SEMESTER VI (12 SKS)</b></td>
+        <td></td>
+        <td></td>
+      </tr>
+        <td>43</td>
+        <td>PBA643</td>
+        <td  class="Matkul">PRAKTEK KERJA LAPANGAN (PKL)</td>
+        <td>4</td>
+        <td>A</td>
+      </tr>
+      <td>44</td>
+        <td>PBA642</td>
+        <td  class="Matkul">SEMINAR</td>
+        <td>2</td>
+        <td>A</td>
+      </tr  >
+      <tr style="border-bottom: 1px solid black">
+      <td>45</td>
+        <td>PBA641</td>
+        <td  class="Matkul">KERTAS KERJA WAJID</td>
+        <td>6</td>
+        <td>B</td>
+      </tr>
+      <tr>
+        <td></td>
+        <td colspan="3" class="Matkul"><b>UJIAN AKHIR PROGRAM STUDI :</b></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td colspan="3" class="Matkul"></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td >46</td>
+        <td class="Matkul" colspan="3">UJIAN LISAN KOMPREHENSIF</td>
+        <td >AB</td>
+      </tr>
+      <tr>
+      </tr>
+      <tr style="border-bottom: 1px solid black">
+        <td></td>
+        <td colspan="3" class="Matkul"></td>
+        <td></td>
+      </tr>
+      </tr>
+        <td></td>
+        <td colspan="4" class="Matkul"><b>JUDUL KERTAS KERJA WAJIB :</b></td>
+      </tr>
+
+      <tr >
+        <td></td>
+        <td  colspan="4" ><i>Pengembangan Sistem Informasi Kepegawaian Menggunakan <br/>Framework  TOGAF </i></td>
+      </tr>
+      <tr style="border-bottom: 1px solid black">
+        <td></td>
+        <td colspan="4" class="Matkul"></td>
+      </tr>
+
+      <!-- TABEL BAWAH -->
+
+      <tr>
+        <td ></td>
+        <td colspan="4"></td>
+      </tr>
+        <tr>
+          <td></td>
+          <td colspan="4" class="Matkul">JUMLAH SKS <t style="margin-left: 150px">: 21 SKS</t></td>
+        </tr>
+        <tr>
+        <td ></td>
+        <td colspan="4"></td>
+      </tr>
+        <tr>
+          <td></td>
+          <td colspan="4" class="Matkul">KUMULATIF <t style="margin-left: 155px">: 3.36</t></td>
+        </tr>
+        <tr>
+        <td ></td>
+        <td colspan="4"></td>
+      </tr>
+        <tr>
+          <td></td>
+          <td colspan="4" class="Matkul">PREDIKAT KELULUSAN <t style="margin-left: 70px">: SANGAT MEMUASKAN</t></td>
+        </tr>
+      <tr style="border-bottom: 1px solid black;  border-bottom: 3px double #000">
+        <td ></td>
+        <td colspan="4"></td>
+      </tr>
+    </table>
+  </div>
+  <p>KETERANGAN <t style="margin-left:20px">: A=4; AB=3.50;B=3;BC=2.50;C=2;D=1;E=0</t></p>
+  <table style="text-align:center; margin-bottom:80px">
+      <tr>
+        <td>
+          <p>Wakil Direktur I</p>
+          <p style="margin-bottom:100px">POLITEKNIK XYZ</p>
+          <p>Nobita Nobi</p>
+          <p>NIP. 123</p>
+        </td>
+        <td class="box" >4 x 6</div></td>
+        <td>          
+          <p>Wakil Direktur I</p>
+          <p style="margin-bottom:100px">POLITEKNIK XYZ</p>
+          <p>Nobita Nobi</p>
+          <p>NIP. 123</p>
+        </td>
+      </tr>
+  </table>
+
+</body>
+</html>
